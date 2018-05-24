ccb4b554646f051100a5986f396b7b6dbaa7ef32
BastardoONe <Pablo__ossandon@hotmail.se>
2018-05-24 10:12:08 +0200

    merge fix

--- app/src/main/java/com/android/project/abcappen/activities/StatisticsActivity.java
+++ app/src/main/java/com/android/project/abcappen/activities/StatisticsActivity.java
@@ -3,11 +3,8 @@ package com.android.project.abcappen.activities;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.widget.ImageView;
-=======
 import android.view.View;
->>>>>>> acfa33ffd7a7943c2066323e9ea3d32c2cdb6663
 import android.widget.TextView;
 
 import com.android.project.abcappen.R;
@@ -22,14 +19,12 @@ public class StatisticsActivity extends AppCompatActivity {
     private ProfileDatabaseHelper profileDatabaseHelper;
     private ArrayList<LetterProgress> writingProgress;
     private String[] letters;
-<<<<<<< HEAD
     private ImageView imageViewAnim;
     AnimationDrawable anim;
-=======
     private int lettersIdx;
     private LetterProgress letterProgress;
     private Sounds sounds;
->>>>>>> acfa33ffd7a7943c2066323e9ea3d32c2cdb6663
+
 
     private TextView tvName, tvLetter, tvTimesCompleted, tvCompletionTime, tvAccuracy;
 
